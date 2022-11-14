01d1d06cba7f31b58087321ef56fcf801b5aab7d
timbabnik <tim.babnik16@gmail.com>
2022-11-14 16:44:31 +0100

    first commit

---
+++ api/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+const app = express()
+import authRoutes from "./routes/auth.js"
+import userRoutes from "./routes/users.js"
+import postRoutes from "./routes/posts.js"
+import commentRoutes from "./routes/comments.js"
+import likeRoutes from "./routes/likes.js"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+app.use((req,res, next) => {
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+})
+app.use(express.json())
+app.use(cors({
+    origin: "http://localhost:3001",
+}))
+app.use(cookieParser())
+
+
+app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/posts", postRoutes)
+app.use("/api/comments", commentRoutes)
+app.use("/api/likes", likeRoutes)
+
+app.listen(8800, () => {
+    console.log("api working");
+})
